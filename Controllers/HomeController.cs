240e7a8a2ee16ef9a94c0898ab11c060c590dbac
Andrea Moldovan <andreamoldovan98@yahoo.com>
2021-10-26 20:19:33 +0300

    Lab4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using Moldovan_Andrea__Lab2.Models;
 using System;
@@ -6,17 +7,42 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Andrea__Lab2.Data;
+using Moldovan_Andrea__Lab2.Models.LibraryViewModels;
+
 
 namespace Moldovan_Andrea__Lab2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+
+        private readonly LibraryContext _context;
+        public HomeController(LibraryContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data =
+            from order in _context.Orders
+            group order by order.OrderDate into dateGroup
+            select new OrderGroup()
+            {
+                OrderDate = dateGroup.Key,
+                BookCount = dateGroup.Count()
+            };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
+      /*  private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+        */
 
         public IActionResult Index()
         {
